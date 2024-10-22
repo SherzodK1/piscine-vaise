b0a7114af39dddf92c61ec34c2eafd1d9cfc98ea
SherzodK1 <156648364+SherzodK1@users.noreply.github.com>
2024-10-22 09:41:03 +0000

    comit prime

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,23 @@
 <script setup>
+
+import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
+
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
